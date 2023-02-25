7675c37fdcd30b587cf8b945c584e75a4abdf792
Erik Hren <hrenerik2@gmail.com>
2023-02-25 11:08:30 +0100

    multiplication test

--- test_calculator.py
+++ test_calculator.py
@@ -7,6 +7,8 @@ import calculator
 
 
 class TestCalculator:
+
+    
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
@@ -14,5 +16,6 @@ class TestCalculator:
     def test_substraction(self):
         assert 2 == calculator.subtract(4, 2)
 
+
     def test_multiplication(self):
-        assert 100 == calculator.multiply(10,10)
+        assert 100 == calculator.multiply(10, 10)
